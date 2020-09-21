4778a891de1de0dd66611570334d5703f9efa508
garciacello <jgarciacello@gmail.com>
2020-09-21 11:38:27 -0300

    Exerci

---
+++ Ex002.py
@@ -0,0 +1,4 @@
+nome=input('Digite seu nome:')
+print('Seja bem vindo', nome)
+print('Prazer em conhece-lo, {}'.format(nome))
+#('Seja bem vindo, {}'.format(nome))
---
+++ Ex003.py
@@ -0,0 +1,5 @@
+N1=int(input('Digite um valor: '))
+N2=int(input('Digite outro valor: '))
+S= N1 + N2
+
+print('A soma entre {} e {} é {}'.format(N1,N2,S))
